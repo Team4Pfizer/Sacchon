208b12b05ec61ca0ae626c5bb74edbd4426a6df1
GZFP <gzfp@icloud.com>
2023-02-04 20:08:42 +0200

    added little test comment

--- Backend/sacchon/src/main/java/gr/codehub/sacchon/SacchonApplication.java
+++ Backend/sacchon/src/main/java/gr/codehub/sacchon/SacchonApplication.java
@@ -9,6 +9,7 @@ public class SacchonApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(SacchonApplication.class, args);
 		//test to commit
+		// test 2 to commit
 	}
 
 }
