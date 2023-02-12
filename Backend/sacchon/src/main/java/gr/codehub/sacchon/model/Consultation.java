e315c34bbf16e03e0c0b85e2f5bcd406d2fbecc0
GZFP <gzfp@icloud.com>
2023-02-12 21:04:42 +0200

    Class has now properties

--- Backend/sacchon/src/main/java/gr/codehub/sacchon/model/Consultation.java
+++ Backend/sacchon/src/main/java/gr/codehub/sacchon/model/Consultation.java
@@ -1,4 +1,25 @@
 package gr.codehub.sacchon.model;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.Getter;
+import lombok.Setter;
+
+import java.util.Date;
+
+@Entity
+@Getter
+@Setter
 public class Consultation {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long consultationId;
+    private String message ; // the consultation message
+    private Date consultationOfMonth; // not sure if valid
+
+
+
 }
