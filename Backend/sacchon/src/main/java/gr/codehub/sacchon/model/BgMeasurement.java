8ffcc1b9b577a521a2c266281d53a0441d4e9b63
GZFP <gzfp@icloud.com>
2023-02-12 20:35:20 +0200

    added NoArgs lombok annotations

--- Backend/sacchon/src/main/java/gr/codehub/sacchon/model/BgMeasurement.java
+++ Backend/sacchon/src/main/java/gr/codehub/sacchon/model/BgMeasurement.java
@@ -5,6 +5,7 @@ import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import lombok.Setter;
 
 import java.sql.Time;
@@ -14,6 +15,7 @@ import java.util.Date;
 @Entity
 @Getter
 @Setter
+@NoArgsConstructor
 public class BgMeasurement {
 
 
