ae1afc120a8faa9fd9f0321a9b8a4347062e6e63
GZFP <gzfp@icloud.com>
2023-02-12 20:20:45 +0200

    added props

--- Backend/sacchon/src/main/java/gr/codehub/sacchon/model/Doctor.java
+++ Backend/sacchon/src/main/java/gr/codehub/sacchon/model/Doctor.java
@@ -1,4 +1,13 @@
 package gr.codehub.sacchon.model;
 
 public class Doctor {
+
+    private Long doctorId ;
+    private String userEmail ;
+    private String userPassword;
+    private String firstName;
+    private String lastName;
+
+
+
 }
