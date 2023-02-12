c7ada6e8a77fa9a962fb628a4749251696c32310
GZFP <gzfp@icloud.com>
2023-02-12 20:34:41 +0200

    Added properties in class and lombok annotations

--- Backend/sacchon/src/main/java/gr/codehub/sacchon/model/BgMeasurement.java
+++ Backend/sacchon/src/main/java/gr/codehub/sacchon/model/BgMeasurement.java
@@ -1,4 +1,34 @@
 package gr.codehub.sacchon.model;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.Getter;
+import lombok.Setter;
+
+import java.sql.Time;
+import java.util.Date;
+
+
+@Entity
+@Getter
+@Setter
 public class BgMeasurement {
+
+
+    //blood glucose level (date, time, measured in mg/dL)
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long measurementId ;
+
+    private double measurementMg;
+    private Date measurementDate;
+
+    private Time measurementTime;
+
+
+
+
 }
