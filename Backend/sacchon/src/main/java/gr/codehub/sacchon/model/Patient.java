54558e7707dc67dd7f2e65c2c3d997ff8826fe37
GZFP <gzfp@icloud.com>
2023-02-12 17:16:04 +0200

    Added patient class blueprint

--- Backend/sacchon/src/main/java/gr/codehub/sacchon/model/Patient.java
+++ Backend/sacchon/src/main/java/gr/codehub/sacchon/model/Patient.java
@@ -1,4 +1,32 @@
 package gr.codehub.sacchon.model;
 
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import lombok.Getter;
+import lombok.Setter;
+
+import java.util.Date;
+
+@Getter
+@Setter
+@Entity
 public class Patient {
+
+    @Id
+    private Long id ;
+    private String firstname ;
+    private String lastname ;
+
+    private Date registrationDate;
+
+    private int age ;
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public Long getId() {
+        return id;
+    }
 }
