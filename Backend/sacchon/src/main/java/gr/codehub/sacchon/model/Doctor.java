7f4f5c894281dad6e066a7008adfa5e62a0e01ee
GZFP <gzfp@icloud.com>
2023-02-12 20:23:35 +0200

    Added Lombok annotations

--- Backend/sacchon/src/main/java/gr/codehub/sacchon/model/Doctor.java
+++ Backend/sacchon/src/main/java/gr/codehub/sacchon/model/Doctor.java
@@ -1,7 +1,21 @@
 package gr.codehub.sacchon.model;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.*;
+
+@Getter
+@Setter
+@Entity
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
 public class Doctor {
 
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long doctorId ;
     private String userEmail ;
     private String userPassword;
