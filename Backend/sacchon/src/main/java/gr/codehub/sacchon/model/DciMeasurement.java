1c1fb0ba4ed6b07406e521cfe7b4f09d7c6e5999
GZFP <gzfp@icloud.com>
2023-02-12 20:28:01 +0200

    Class now contains annotations and properties

--- Backend/sacchon/src/main/java/gr/codehub/sacchon/model/DciMeasurement.java
+++ Backend/sacchon/src/main/java/gr/codehub/sacchon/model/DciMeasurement.java
@@ -1,4 +1,25 @@
 package gr.codehub.sacchon.model;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Entity
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
 public class DciMeasurement {
+
+    @Id
+    @GeneratedValue( strategy = GenerationType.IDENTITY)
+    private Long measurementId;
+
+    private double dciMeasurementData;
+
 }
