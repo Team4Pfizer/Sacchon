a6d8719f250b5c78db89fd7497ec9cfad7b24857
Panagiota Kleidaria <panagiwtakl1@gmail.com>
2023-02-12 19:06:21 +0200

    First controller attempt

---
+++ Backend/sacchon/src/main/java/gr/codehub/sacchon/controller/DoctorAdviceController.java
@@ -0,0 +1,7 @@
+package gr.codehub.sacchon.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+@RestController
+public class DoctorAdviceController {
+}
