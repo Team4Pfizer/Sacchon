8c4fa169d9e9b06cc5e046740b738458da1df8f0
Georgios Zacharidis <gzfp@icloud.com>
2023-02-15 10:35:40 +0200

    Created ChiefDoctor properties

--- Backend/sacchon/src/main/java/gr/codehub/sacchon/model/ChiefDoctor.java
+++ Backend/sacchon/src/main/java/gr/codehub/sacchon/model/ChiefDoctor.java
@@ -1,4 +1,26 @@
 package gr.codehub.sacchon.model;
 
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@Entity
+@AllArgsConstructor
+@NoArgsConstructor
 public class ChiefDoctor {
+
+    @Id
+    private Long id;
+    private String name;
+    private String surname;
+    private String username;
+    private String password;
+
+
 }
