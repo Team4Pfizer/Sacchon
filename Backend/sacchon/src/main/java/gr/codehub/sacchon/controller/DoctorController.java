ab986293eda2f99d722d50f3cd142ba16341003c
Georgios Zacharidis <gzfp@icloud.com>
2023-02-15 11:21:27 +0200

    added simple mapping to test if the controller works . it works :D

--- Backend/sacchon/src/main/java/gr/codehub/sacchon/controller/DoctorController.java
+++ Backend/sacchon/src/main/java/gr/codehub/sacchon/controller/DoctorController.java
@@ -1,7 +1,6 @@
 package gr.codehub.sacchon.controller;
 
-import lombok.Getter;
-import org.springframework.stereotype.Controller;
+
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
