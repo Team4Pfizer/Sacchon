2f018e92462c4c2b2f476e4fb29282853286e224
Georgios Zacharidis <gzfp@icloud.com>
2023-02-15 11:10:20 +0200

    Added header comment for clarification

--- Backend/sacchon/src/main/java/gr/codehub/sacchon/controller/BgMeasurementController.java
+++ Backend/sacchon/src/main/java/gr/codehub/sacchon/controller/BgMeasurementController.java
@@ -1,4 +1,8 @@
 package gr.codehub.sacchon.controller;
-
+/*
+    * This class is responsible for the BgMeasurementController
+    * it will return a list with all the BgMeasurements
+    * it will return a list with all the BgMeasurements of a specific patient
+ */
 public class BgMeasurementController {
 }
