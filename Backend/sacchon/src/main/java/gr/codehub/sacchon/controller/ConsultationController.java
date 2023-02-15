c2aaf3f98985b9ef17eafe0f2c8048ebf7a798b1
Georgios Zacharidis <gzfp@icloud.com>
2023-02-15 11:07:54 +0200

    Refactored ConsultationController

--- Backend/sacchon/src/main/java/gr/codehub/sacchon/controller/ConsultationController.java
+++ Backend/sacchon/src/main/java/gr/codehub/sacchon/controller/ConsultationController.java
@@ -1,7 +1,6 @@
 package gr.codehub.sacchon.controller;
 
-import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 @RestController
-public class DoctorAdviceController {
+public class ConsultationController {
 }
