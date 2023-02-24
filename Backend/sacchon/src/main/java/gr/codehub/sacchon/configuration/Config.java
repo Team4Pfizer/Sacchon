01b62e2bcc65a2cfd93f1ff004f6a8a323d2c526
StathisStav <stavridisefs@gmail.com>
2023-02-24 14:18:00 +0200

    bug fix config

--- Backend/sacchon/src/main/java/gr/codehub/sacchon/configuration/Config.java
+++ Backend/sacchon/src/main/java/gr/codehub/sacchon/configuration/Config.java
@@ -47,15 +47,22 @@ public class Config {
 
             ) {
         return args -> {
-            Patient patient1 = new Patient(0L,"michael.lawson@gmail.com","Michael","lawson",false,null);
-            Patient patient2 = new Patient(0L,"lindsay.ferguson@gmail.com","Lindsay","Ferguson",false,null);
-
             Doctor doctor1 = new Doctor(0L,"byron.fields@gmail.com","Byron","Fields",null);
             Doctor doctor2 = new Doctor(0L,"tobias.funke@gmail.com","Tobias","Funke",null);
-            patientRepository.save(patient1);
-            patientRepository.save(patient2);
             doctorRepository.save(doctor1);
             doctorRepository.save(doctor2);
+
+            Optional<Doctor> doctor = doctorRepository.findById(1L);
+
+            Patient patient1 = new Patient(0L,"michael.lawson@gmail.com","Michael","lawson",false,doctor.get());
+            Patient patient2 = new Patient(0L,"lindsay.ferguson@gmail.com","Lindsay","Ferguson",false,null);
+
+
+            patientRepository.save(patient1);
+            patientRepository.save(patient2);
+
+            Optional<Patient> patient = patientRepository.findById(1L);
+
 
 
 
@@ -65,18 +72,18 @@ public class Config {
                         0L,
                         120.00,
                         LocalDate.now(movableClock),
-                        LocalTime.now(movableClock), patient1);
+                        LocalTime.now(movableClock), patient.get());
                 DciMeasurement dciMeasurement = new DciMeasurement(
                         0L,
                         250.5,
                         LocalDate.now(movableClock),
-                        patient1);
+                        patient.get());
                 bgMeasurementRepository.save(bgMeasurement);
                 dciMeasurementRepository.save(dciMeasurement);
 
             }
             Clock movableClock = Clock.offset(clock, Duration.ofDays(-1));
-            Consultation consultation = new Consultation(0L,1.5,"Insulin",LocalDate.now(movableClock),patient1);
+            Consultation consultation = new Consultation(0L,1.5,"Insulin",LocalDate.now(movableClock),patient.get());
 
             consultationRepository.save(consultation);
 
