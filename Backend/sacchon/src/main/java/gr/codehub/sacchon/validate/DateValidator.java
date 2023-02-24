152d9bafc4cf54fd9d67166fa0f7d44bc6a0cd46
StathisStav <stavridisefs@gmail.com>
2023-02-24 17:45:30 +0200

    Date Validator

--- Backend/sacchon/src/main/java/gr/codehub/sacchon/validate/DateValidator.java
+++ Backend/sacchon/src/main/java/gr/codehub/sacchon/validate/DateValidator.java
@@ -3,6 +3,7 @@ package gr.codehub.sacchon.validate;
 import gr.codehub.sacchon.exception.BadRequestException;
 
 import java.time.LocalDate;
+import java.time.LocalTime;
 import java.time.format.DateTimeFormatter;
 import java.time.format.DateTimeParseException;
 
@@ -14,8 +15,15 @@ public class DateValidator {
         } catch (DateTimeParseException e) {
             throw new BadRequestException("You did not provide a date. Input : "+date);
         }
-
-
-
     }
+    public static LocalTime validateTime(String time)throws BadRequestException{
+        DateTimeFormatter dateFormatter = DateTimeFormatter.ofPattern("HHmm");
+        try {
+            return LocalTime.parse(time, dateFormatter);
+        } catch (DateTimeParseException e) {
+            throw new BadRequestException("You did not provide a Time. Input : "+time);
+        }
+    }
+
+
 }
